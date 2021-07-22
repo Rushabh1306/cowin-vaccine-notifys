25a1058d3030afd382a422ceb08d9a2e2a02a082
Rushabhsoni1306 <64094208+Rushabh1306@users.noreply.github.com>
2021-07-22 15:56:45 +0530

    Updated Settings.py

--- cowin_vaccine/settings.py
+++ cowin_vaccine/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 from decouple import config
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = "django-insecure-215%29ewp1-fqu$p$b_3*oqg5+!+t(k5-qyjnmr(#0p@z!3br7"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
