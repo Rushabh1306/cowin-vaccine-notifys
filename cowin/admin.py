a54a1f1898b8fd24740b3352f6eb7ef62f409445
Rushabhsoni1306 <64094208+Rushabh1306@users.noreply.github.com>
2021-07-22 16:32:12 +0530

    Updated Django-Heroku

--- cowin/admin.py
+++ cowin/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import Filter, StatesList,DistrictList, UserFilter
+from .models import StatesList,DistrictList, UserFilter
 # Register your models here.
 admin.site.register(StatesList)
 admin.site.register(DistrictList)
-admin.site.register(Filter)
 admin.site.register(UserFilter)
--- cowin/forms.py
+++ cowin/forms.py
@@ -1,16 +1,6 @@
-from .models import Filter, UserFilter
+from .models import UserFilter
 from django.contrib.auth.models import User
 from django.forms import ModelForm
-
-class UserForm(ModelForm):
-    class Meta:
-        model = User
-        fields = ('email',)
-
-class FilterForm(ModelForm):
-	class Meta: 
-		model = Filter
-		fields = ('age','vaccine_type','vaccine_name','pincode')
 
 class UserFilterForm(ModelForm):
 	class Meta: 
